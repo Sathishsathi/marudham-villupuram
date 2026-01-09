87c1d9b5aa578ffbb71ec999e51d96e77ba4cc86
Vasanth2005kk <vasanth2005kk@gmail.com>
2026-01-09 18:35:48 +0530

    unwanted files and images deleted !

---
+++ assets/js/portfolio-data.js
@@ -0,0 +1,167 @@
+window.portfolioData = {
+    "settings": {
+        "defaultFilter": ".filter-2024"
+    },
+    "filters": [
+        { "name": "2024", "filter": ".filter-2024" },
+        { "name": "2020", "filter": ".filter-2020" }
+    ],
+    "items": [
+        {
+            "category": "filter-2024",
+            "image": "assets/img/portfolio/473328996_1157930685694812_4061481445079322158_n.jpg",
+            "title": "மருதம் விழா 2024",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2024",
+            "image": "assets/img/portfolio/473421870_2706645526203056_3411609979371753768_n.jpg",
+            "title": "மருதம் விழா 2024",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2024",
+            "image": "assets/img/portfolio/473429548_2706645059536436_3071468070260088839_n.jpg",
+            "title": "மருதம் விழா 2024",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2024",
+            "image": "assets/img/portfolio/473593531_2706644996203109_6262477552640267557_n.jpg",
+            "title": "மருதம் விழா 2024",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2024",
+            "image": "assets/img/portfolio/473598633_10233109960445530_5415568365050656432_n.jpg",
+            "title": "மருதம் விழா 2024",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2024",
+            "image": "assets/img/portfolio/473598772_2706645092869766_3508772119376365848_n.jpg",
+            "title": "மருதம் விழா 2024",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2024",
+            "image": "assets/img/portfolio/473617995_2717946298376017_4472487627121890700_n.jpg",
+            "title": "மருதம் விழா 2024",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82001469_1307285576108770_6247288587847467008_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82052996_1307266272777367_6534819653129076736_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82250799_1307238939446767_4371025901182255104_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82331699_1308641492639845_1750868002607202304_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82459437_1307266592777335_3640484734733123584_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82476815_1238287159690622_7251791725104988160_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82577128_1307280269442634_5608059048881553408_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82603257_1308105816026746_640306643749306368_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82604513_1307200732783921_3367721673980641280_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82604704_1308751542628840_1786393386509402112_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82856635_1307151186122209_2927286190958706688_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82860711_1308283276009000_3647112178508496896_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/82881988_1307200302783964_1665940972858507264_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/83108474_1308107369359924_4758167050583539712_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/83119231_1308641592639835_3198481507043770368_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/83253993_1307244142779580_2739368413842374656_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/83281417_1307200512783943_1858214976389382144_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/83312690_1308273339343327_7522860468503838720_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        },
+        {
+            "category": "filter-2020",
+            "image": "assets/img/portfolio/83445635_1308306879339973_1130957737639804928_n.jpg",
+            "title": "மருதம் விழா 2020",
+            "description": "மருதம் நிகழ்ச்சி காட்சிகள்"
+        }
+    ]
+};
